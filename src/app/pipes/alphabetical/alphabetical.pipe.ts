95bcae3b8c27f8b1c89894ee04131ac5535a81eb
Peter Duff <peterduff@gmail.com>
2023-05-10 11:18:45 +0100

    RNMS-52 Reverse alphabetical

--- src/app/pipes/alphabetical/alphabetical.pipe.ts
+++ src/app/pipes/alphabetical/alphabetical.pipe.ts
@@ -5,33 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AlphabeticalPipe implements PipeTransform {
 
-    transform(items: any[], key?): any {
+    transform(items: any[], key): any {
         if (!items) {
             return [];
         }
 
-        if (key) {
-            items = items.sort(function (a, b) {
-                if (a[key] > b[key]) {
-                    return 1;
-                }
+        items = items.sort(function (a, b) {
+            if (a[key] > b[key]) {
+                return 1;
+            }
 
-                if (a[key] < b[key]) {
-                    return -1;
-                }
-            });
-        } else {
-            items = items.sort(function (a, b) {
-                if (a > b) {
-                    return 1;
-                }
-
-                if (a < b) {
-                    return -1;
-                }
-            });
-        }
-
+            if (a[key] < b[key]) {
+                return -1;
+            }
+        });
 
         return items;
     }
