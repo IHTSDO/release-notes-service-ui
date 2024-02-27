5f058e0217549a312d9d4ba696b273fcc38fc6a8
Peter Duff <peterduff@gmail.com>
2024-02-27 16:49:03 +0000

    RNMS-93 ISRS table test

---
+++ src/app/services/jira/jira.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {Observable, Subject, Subscription} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {ReleaseNotesService} from "../releaseNotes/release-notes.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class JiraService {
+
+    activeVersion: any;
+    activeVersionSubscription: Subscription;
+
+    knownJiraIssues = new Subject();
+    resolvedJiraIssues = new Subject();
+
+    constructor(private http: HttpClient,
+                private releaseNotesService: ReleaseNotesService) {
+        this.activeVersionSubscription = this.releaseNotesService.getActiveVersion().subscribe(data => this.activeVersion = data);
+    }
+
+    setKnownJiraIssues(knownJiraIssues) {
+        this.knownJiraIssues.next(knownJiraIssues);
+    }
+
+    getKnownJiraIssues() {
+        return this.knownJiraIssues.asObservable();
+    }
+
+    setResolvedJiraIssues(resolvedJiraIssues) {
+        this.resolvedJiraIssues.next(resolvedJiraIssues);
+    }
+
+    getResolvedJiraIssues() {
+        return this.resolvedJiraIssues.asObservable();
+    }
+
+    httpGetKnownJiraIssues(): Observable<any> {
+        return this.http.get<any>('/jira/issue/picker?query=“INT 20240201 - Known Issues (On Hold)” ORDER BY key ASC');
+    }
+
+    httpGetResolvedJiraIssues(): Observable<any> {
+        return this.http.get<any>('/jira/issue/picker?query=“INT 20240201 - Resolved Issues” ORDER BY key ASC');
+    }
+}
