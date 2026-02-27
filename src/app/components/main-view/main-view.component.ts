9b5fde5bec2eec2c3a0eaab761cd4f70240c945e
Quyen Ly <quocquyenit@gmail.com>
2026-02-27 12:09:12 +0700

    RNMS-122 Fix issue that the additional lines are removed on saving

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -1,16 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, signal, ViewChild} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {ReleaseNotesService} from '../../services/releaseNotes/release-notes.service';
 import {AuthenticationService} from '../../services/authentication/authentication.service';
 import {ModalService} from '../../services/modal/modal.service';
 import {ToastrService} from 'ngx-toastr';
-import { MarkdownService, MarkdownComponent } from "ngx-markdown";
+import { MarkdownComponent } from "ngx-markdown";
 import Quill from 'quill';
 import TurndownService from 'turndown';
-import {JiraService} from "../../services/jira/jira.service";
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from '../modal/modal.component';
 import { NoteWithContentFilterPipe } from '../../pipes/note-filter/note-with-content-filter.pipe';
+import MarkdownIt from 'markdown-it';
 
 @Component({
     selector: 'app-main-view',
@@ -19,6 +19,8 @@ import { NoteWithContentFilterPipe } from '../../pipes/note-filter/note-with-con
     imports: [CommonModule, MarkdownComponent, ModalComponent, NoteWithContentFilterPipe]
 })
 export class MainViewComponent implements OnInit {
+
+    @ViewChild('editor') editorRef!: ElementRef;
 
     toastrConfig = {
         closeButton: true
@@ -39,8 +41,15 @@ export class MainViewComponent implements OnInit {
     editMode: any;
     editModeSubscription: Subscription;
 
-    quill: any;
-    turndown: any;
+    quill: Quill;
+    md = new MarkdownIt({ html: true, breaks: true });
+    turndown = new TurndownService({
+        bulletListMarker: '-'
+    });
+
+    // Angular 20 signal
+    markdown = signal('');
+
     toolbarOptions = [
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
         ['bold', 'italic', 'link'],
@@ -51,9 +60,7 @@ export class MainViewComponent implements OnInit {
     constructor(private releaseNotesService: ReleaseNotesService,
                 private authenticationService: AuthenticationService,
                 private modalService: ModalService,
-                private jiraService: JiraService,
-                private toastr: ToastrService,
-                private markdownService: MarkdownService) {
+                private toastr: ToastrService) {
         this.activeVersionSubscription = this.releaseNotesService.getActiveVersion().subscribe(data => {
             this.activeVersion = data;
             this.releaseNotesService.httpGetReleaseNotes().subscribe(notes => {
@@ -81,13 +88,9 @@ export class MainViewComponent implements OnInit {
     }
 
     save(): void {
-        let content = this.quill.root.innerHTML;
-        // this.activeReleaseNote.content = this.markdownService.parse(content);
-
-        const turndownService = new TurndownService();
-
-        this.activeReleaseNote.content = turndownService.turndown(content);
-
+        const content = this.quill.root.innerHTML;
+        const normalizedHtml = this.normalizeQuillLists(content);
+        this.activeReleaseNote.content = this.turndown.turndown(normalizedHtml);
         this.releaseNotesService.httpPutReleaseNote(this.activeReleaseNote).subscribe(data => {
             this.releaseNotesService.setEditedContent(false);
             this.toastr.success('Release Note: ' + data['title'], 'SAVED', this.toastrConfig);
@@ -111,16 +114,27 @@ export class MainViewComponent implements OnInit {
     }
 
     quillInit(): void {
-        this.quill = new Quill('#quill-editor', { modules: { toolbar: this.toolbarOptions }, theme: 'snow'});
-
-        let html = this.markdownService.parse(this.activeReleaseNote.content);
-        // let content = html?.endsWith('\n<p><br></p>') ? html + '\n<p><br></p>' : html;
-        // this.quill.clipboard.dangerouslyPasteHTML(content);
-        this.quill.clipboard.dangerouslyPasteHTML(html);
-        this.quill.on('text-change', () => {
-            this.releaseNotesService.setEditedContent(true);
-            this.releaseNotesService.setContent(this.quill.root.innerHTML);
-        });
+        this.quill = new Quill(this.editorRef.nativeElement, { modules: { toolbar: this.toolbarOptions, keyboard: {
+                bindings: {
+                    softbreak: {
+                    key: 13,
+                    shiftKey: true,
+                    handler: (range: any) => {
+                        const quill = this.quill;
+                        quill.insertEmbed(range.index, 'break', true, 'user');
+                        quill.setSelection(range.index + 1, 0);
+                        return false;
+                    }
+                    }
+                }
+            } 
+            }, theme: 'snow'});
+                this.syncToRichText();
+                this.quill.on('text-change', () => {
+                    this.releaseNotesService.setEditedContent(true);
+                    this.releaseNotesService.setContent(this.quill.root.innerHTML);
+            }
+        );
     }
 
     roleContains(role): boolean {
@@ -158,4 +172,79 @@ export class MainViewComponent implements OnInit {
             this.toastr.error('Release notes failed to version', 'FAILURE');
         });
     }
+
+    normalizedMarkdownContent(content: string): string {
+        const rawContent = content ?? '';
+        return rawContent
+            // literal "\n\n" → <br> for a visible blank line
+            .replace(/\s\n\n\s/g, '<br>');
+    }
+
+    private normalizeQuillLists(html: string): string {
+        if (!html) {
+            return '';
+        }
+
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const body = doc.body;
+
+        const processOrderedList = (list: HTMLOListElement) => {
+            const parent = list.parentElement;
+            if (!parent) {
+                return;
+            }
+
+            const items = Array.from(list.children).filter(
+                (el): el is HTMLLIElement => el.tagName === 'LI'
+            );
+
+            if (!items.length) {
+                return;
+            }
+
+            const fragment = doc.createDocumentFragment();
+            let currentList: HTMLOListElement | HTMLUListElement | null = null;
+            let currentType: 'bullet' | 'ordered' | null = null;
+
+            items.forEach(item => {
+                const typeAttr = item.getAttribute('data-list');
+                const type: 'bullet' | 'ordered' =
+                    (typeAttr === 'bullet' || typeAttr === 'ordered')
+                        ? typeAttr
+                        : 'ordered';
+
+                if (!currentList || type !== currentType) {
+                    currentType = type;
+                    currentList = doc.createElement(type === 'bullet' ? 'ul' : 'ol') as
+                        | HTMLOListElement
+                        | HTMLUListElement;
+                    fragment.appendChild(currentList);
+                }
+
+                const li = item.cloneNode(true) as HTMLLIElement;
+                li.removeAttribute('data-list');
+                currentList.appendChild(li);
+            });
+
+            parent.replaceChild(fragment, list);
+        };
+
+        const orderedLists = Array.from(body.querySelectorAll('ol'));
+        orderedLists.forEach(list => processOrderedList(list as HTMLOListElement));
+
+        return body.innerHTML;
+    }
+
+    // Markdown → Quill
+    private syncToRichText() {
+        if (!this.quill) {
+            return;
+        }
+        console.log( this.activeReleaseNote?.content);
+        const normalizedMarkdownContent = this.normalizedMarkdownContent(this.activeReleaseNote?.content);
+        let html = this.md.render(normalizedMarkdownContent);
+        this.quill.clipboard.dangerouslyPasteHTML(html);
+    }    
+
 }
